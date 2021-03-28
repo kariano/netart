5945c24421d6b7c5674422160975c2291b98ed31
Marco Ariano <marco.ariano@lightbend.com>
2021-03-28 13:55:11 -0700

    Add geolocation demo

--- testing/sketch_testing.js
+++ testing/sketch_testing.js
@@ -25,9 +25,36 @@ function onErrorFetch() {
 //
 //// end of IP address business
 
+//// Geolocation business
+//
+
+let myLatitude;
+let myLongitude;
+
+function gotLocation(position){
+    // This shows all the stuff that you might get.
+    // You'd have to test they have useful values before using though, cuz
+    // you don't always get all the info.
+    print("lat: " + position.latitude);
+    print("long: " + position.longitude);
+    print("accuracy: " + position.accuracy)
+    print("altitude: " + position.altitude)
+    print("altitude accuracy: " + position.altitudeAccuracy)
+    print("heading: " + position.heading)
+    print("speed: " + position.speed)
+
+    myLatitude = position.latitude;
+    myLongitude = position.longitude;
+}
+
+//
+//// end of IP address business
+
 function setup() {
     //console.log("In setup");
+
     getIPAddress();
+    getCurrentPosition(gotLocation)
 
     createCanvas(400, 400);
 }
@@ -36,6 +63,10 @@ function draw() {
     if (myIPAddress) {
         text("My IP address is " + myIPAddress, 20, 60);
     }
-    text("This is some text", 40, 80);
+    if (myLatitude && myLongitude) {
+        text("My latitude is " + myLatitude, 20, 80);
+        text("My longitude is " + myLongitude, 20, 100);
+    }
+    text("This is some text", 20, 120);
     //background(220);
 }
